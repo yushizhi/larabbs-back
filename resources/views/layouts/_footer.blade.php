be89ef57bfe997e0a52d8b32ace25030f3aba183
vagrant <vagrant@homestead>
2018-07-10 11:26:43 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://github.com/yushizhi/" target="_blank">Ysz</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="yushizhi@live.com">关于我们</a></p>
+    </div>
+</footer>
