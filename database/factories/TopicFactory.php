027f7d8130bba639636121046bea1746a55f7d11
vagrant <vagrant@homestead>
2018-07-12 09:02:58 +0000

    Seeding topics and users data

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $updated_at = $faker->dateTimeThisMonth();
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $faker->sentence(),
+        'body' => $faker->text(),
+        'excerpt' => $faker->sentence(),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
