b95b28e64a570bbdfe09da5337dba57547dd795c
vagrant <vagrant@homestead>
2018-07-24 07:13:25 +0000

    Translate Rest Passwd Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">登 人们rm录</div>
+                <div class="panel-heading">登 录</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
